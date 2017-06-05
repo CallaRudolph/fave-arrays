118dc1ea22b4ff26209ae844402daf857c57a1b1
Calla Rudolph and Larry Taylor <calla@>
2017-06-05 09:10:55 -0700

    add html content, define variables

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  var cheese = $("input#cheese").val();
+  var animal = $("input#animal").val();
+  var larry = $("input#larry").val();
+  var max = $("input#max").val();
+  var brady = $("input#brady").val();
+
+  $(".cheese").text(cheese);
+  $(".animal").text(animal);
+  $(".larry").text(larry);
+  $(".max").text(max);
+  $(".brady").text(brady);
+})
